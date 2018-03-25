94a85e00f13ee14c74eeac7d87ccdd8a641f0bb5
Ryo Ota <nwtgck@gmail.com>
2018-03-25 12:31:13 +0900

    Add some tests to ensure that OverloadedStrings works

--- test/Data/String/HereSpec.hs
+++ test/Data/String/HereSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Data.String.HereSpec where
 
@@ -7,6 +8,8 @@ import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
 import qualified Data.Char as Char
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
 
 import Data.String.Here
 
@@ -17,22 +20,48 @@ spec :: Spec
 spec = do
   describe "i quote" $ do
     it "should interpolate a number" $ do
-      let val1   = 7878 :: Int
+      let val1 :: Int
+          val1   = 7878 
+          expect :: String
           expect = "value is 7878"
+          actual :: String
           actual = [i|value is ${val1}|]
       actual `shouldBe` expect
 
     -- (from: https://github.com/tmhedberg/here#readme)
     it "should interpolate a String expression" $ do
-      let foo = "foo"
+      let foo :: String
+          foo = "foo"
+          expect :: String
           expect = "\"foo\", when capitalized, is FOO!"
+          actual :: String
           actual = [i|"foo", when capitalized, is ${map Char.toUpper foo}!|]
+      actual `shouldBe` expect
+      
+    it "should interpolate a number to a ByteString" $ do
+      let val1 :: Int
+          val1   = 3535
+          expect :: BS.ByteString
+          expect = "value is 3535" 
+          actual :: BS.ByteString
+          actual = [i|value is ${val1}|]
+      actual `shouldBe` expect
+    
+    it "should interpolate a number to a Text" $ do
+      let val1 :: Int
+          val1   = 9988
+          expect :: T.Text
+          expect = "value is 9988" 
+          actual :: T.Text
+          actual = [i|value is ${val1}|]
       actual `shouldBe` expect
   
   -- (from: https://github.com/tmhedberg/here#readme)
   describe "here quote" $ do
     it "should be here docs" $ do
-      let expect = "Hello world,\n\nI am a multiline here doc!"
+      let expect :: String
+          expect = "Hello world,\n\nI am a multiline here doc!"
+          actual :: String
           actual = [here|
 Hello world,
 
